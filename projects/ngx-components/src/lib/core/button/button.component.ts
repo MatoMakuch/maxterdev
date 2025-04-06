26771213b50556f32f67e1d171eca25e478ca4b7
MatoMakuch <m.makuch03@gmail.com>
2025-04-06 21:07:29 +0200

    Refactor styles to use CSS custom properties across components, update button and input styles, and remove unused SVG asset; add button component styles and integrate theme variables for consistent theming

--- projects/ngx-components/src/lib/core/button/button.component.ts
+++ projects/ngx-components/src/lib/core/button/button.component.ts
@@ -3,9 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'maxterdev-button',
   standalone: true,
-  templateUrl: './button.component.html'
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
+  @Input() disabled: boolean = false;
   @Input() data: any;  // Accept data input
 
   onButtonClick() {
